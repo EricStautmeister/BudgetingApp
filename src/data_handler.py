a320f675e9e6e729b37245edf9d940b68922498f
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2022-11-04 11:13:53 +0100

    filter algorithm implemented more efficiently

--- src/data_handler.py
+++ src/data_handler.py
@@ -14,20 +14,17 @@ class DataHandler():
     def save_data(self, data: dict[str, dict[str, str | int]]) -> None:
         """
         This function takes in a dictionary with a dictionary, 
-        and updates the budget data file with the new data in json format.
+        and updates the budget data file with the new data into json format.
 
         :param data: dict[str, dict[str, str | int]]
         :type data: dict[str, dict[str, str | int]]
         """
         try:
-            # load current data
-            loaded_json = json.load(f)
-            # update current data with new data
-            loaded_json.update(data)
-
-            # dump updated data to data file
-            with open("budget_data.json", "w") as f:
-                json.dump(loaded_json, f)
+            with open("budget_data.json", "r") as f:
+                current_budget_data = json.load(f)
+                current_budget_data.update(data)
+                with open("budget_data.json", "w") as f:
+                    json.dump(current_budget_data, f)
         except Exception as e:
             # if no data exists, create new data file
             json.dump(data, f)
@@ -46,24 +43,28 @@ class DataHandler():
             return json.load(f)
     
     def format_data(self, data: dict[str, dict[str, str | int]]) -> list[str]:
-        return [f"Name: {key}\nCategory: {value['category']}\nTimeframe: {value['timeframe']}\nAmount: {value['budget-amount']}\n\n" for key, value in data.items()]
+        keyList = [f"{key}" for key in data]
+        formated_data_list = [f"Name: {key}\nCategory: {value['category']}\nTimeframe: {value['timeframe']}\nAmount: {value['budget-amount']}\n\n" for key, value in data.items()]
+        return {"keyList":keyList, "formatted_data_list":formated_data_list}
 
-    def filter_data(self, values, dict: dict):
+    def filter_data(self, values, dict: dict[str, dict[str, str | int]]):
         category, timeframe = values["category"].upper(
         ), values["timeframe"].upper()
         filtered_dict = {}
-        if category == '' or category == 'ALL' and timeframe == '' or timeframe == 'ALL':
-            filtered_dict = dict
-        elif category in ['', 'ALL']:
-            for key, value in dict.items():
-                if value["timeframe"].upper() == timeframe:
-                    filtered_dict[key] = value
-        elif timeframe in ['', 'ALL']:
-            for key, value in dict.items():
-                if value["category"].upper() == category:
-                    filtered_dict[key] = value
-        else:
-            for key, value in dict.items():
-                if value["category"].upper() == category and value["timeframe"].upper() == timeframe:
-                    filtered_dict[key] = value
+        print(category, timeframe)
+        if category == "ALL" and timeframe == "ALL":
+            return dict
+        if category == "" and timeframe == "":
+            return dict
+        if category == "ALL" and timeframe == "":
+            return dict
+        if category == "" and timeframe == "ALL":
+            return dict
+        for key, value in dict.items():
+            if category in ['', 'ALL'] and value["timeframe"].upper() == timeframe:
+                filtered_dict[key] = value
+            elif timeframe in ['', 'ALL'] and value["category"].upper() == category:
+                filtered_dict[key] = value
+            elif value["category"].upper() == category and value["timeframe"].upper() == timeframe:
+                filtered_dict[key] = value
         return filtered_dict
